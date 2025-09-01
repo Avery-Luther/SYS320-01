2ba37ef4d69908141b793c4bf7127ba5c6f05761
Avery-Luther <avery.luther@mymail.champlain.edu>
2025-09-01 12:43:27 -0400

    Started BasicLab1.ps1

---
+++ week2/BasicLab1.ps1
@@ -0,0 +1,24 @@
+﻿#(Get-NetIPAddress -AddressFamily IPv4 | Where-Object {($_.InterfaceAlias -ilike "Ethernet")}).IPAddress
+#(Get-NetIPAddress -AddressFamily IPv4 | Where-Object {($_.InterfaceAlias -ilike "Ethernet")}).PrefixLength
+#Get-WmiObject -List | Where-Object { $_.Name -ilike "Win32_net*"} | Sort-Object 
+ 
+#Get-CimInstance Win32_NetworkAdapterConfiguration | select DHCPServer | Format-Table -HideTableHeaders 
+(Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -ilike "Ethernet"}).ServerAddresses[0]
+
+cd $PSScriptRoot
+
+$files=(Get-ChildItem)
+
+for ($j=0; $j -le $files.Length; $j++){
+    if ($files[$j].Name -ilike "*ps1"){
+        Write-Host $files[$j].Name
+    }
+}
+
+$folderpath="$PSScriptRoot\outfolder"
+if (Test-Path -Path $folderpath){
+    Write-Host "Folder Already Exists"
+}
+else{
+    New-Item -Path $folderpath -ItemType Directory
+}
