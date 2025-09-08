1000e14c5a31bbba21edd6f84de50f916a69f939
Avery-Luther <avery.luther@mymail.champlain.edu>
2025-09-07 23:52:21 -0400

    fixing a comment

--- week2/ProcessManagement/Deliverable4.ps1
+++ week2/ProcessManagement/Deliverable4.ps1
@@ -1,5 +1,4 @@
-﻿# A powershell script that lists every service that is stopped then outputted to a CSV
-# Quick note: This lab specifies processes not services, but stopped processes would not show up with Get-Process which only gets running prorcesses. 
+﻿# A powershell script that detects if chorme is open, then if it is closes it, and if it isnt opens it to champlain.edu 
 
 if (Get-process | Where-Object {$_.Name -eq 'chrome'}){
     Stop-Process -Name 'chrome'
