8d187b60ebafe99b1ee79155746ff7dc60578dcf
Avery-Luther <avery.luther@mymail.champlain.edu>
2025-09-22 12:49:01 -0400

    Parsing Apache Logs Complete

---
+++ week4/Parsing-Apache-Logs.ps1
@@ -0,0 +1,18 @@
+﻿function apacheLogs2(){
+    $logsNotFormated = Get-Content C:\xampp\apache\logs\access.log
+    $tableRecords = @()
+    for($i=0; $i -lt $logsNotFormated.Count; $i++){
+        $words = $logsNotFormated[$i].Split(" ");
+        
+        $tableRecords += [PSCustomObject]@{ "IP" = $words[0]; `
+                                            "Time" = $words[3].Trim("["); `
+                                            "Method" = $words[5].Trim('"'); `
+                                            "Page" = $words[6]; `
+                                            "Protocol" = $words[7].Trim('"'); `
+                                            "Resopnse" = $words[8];`
+                                            "Referrer" = $words[10];
+                                            "Client" = $words[11..($words.Length)];
+                                            }
+    }
+return $tableRecords
+}
--- week4/main.ps1
+++ week4/main.ps1
@@ -1,3 +1,7 @@
 ﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+. (Join-Path $PSScriptRoot Parsing-Apache-Logs.ps1)
+Write-Host "Assignment 1:" | Out-String
+Write-Host (apacheLogs "index.html" 200 "Chrome" | Format-Table -AutoSize -Wrap | Out-String)
 
-apacheLogs "index.html" 200 "Chrome"
+Write-Host "Assignment 2:" | Out-String
+Write-Host (apacheLogs2 | Format-Table -Autosize -Wrap | Out-String)
