248a92dbd8b50b54dc658e18205fb4833fd240a6
Avery-Luther <113251295+Avery-Luther@users.noreply.github.com>
2025-11-10 10:31:50 -0500

    Update Email.ps1

--- week7/Email.ps1
+++ week7/Email.ps1
@@ -3,7 +3,7 @@
     $To = "avery.luther@mymail.champlain.edu"
     $Subject = "Suspicious Activity"
 
-    $Password = "obys lgdc guxs laeb" | ConvertTo-SecureString -AsPlainText -Force
+    $Password = "password" | ConvertTo-SecureString -AsPlainText -Force
     $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
 
     Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer "smtp.gmail.com" `
